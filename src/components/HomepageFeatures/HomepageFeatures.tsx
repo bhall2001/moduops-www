030ae12a8cf947ce4fe9960c9d2e6d034a1ffca8
Bob Hall <bhall_2001@yahoo.com>
2022-03-21 18:45:55 -0400

    feat: install docusaurus

---
+++ src/components/HomepageFeatures/HomepageFeatures.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Modular and Home Layouts',
+    Svg: require('@site/static/img/moduops-mac.svg').default,
+    description: (
+      <>
+        Designed for operations on modular layouts such as Free-mo and home layouts of all sizes.
+      </>
+    ),
+  },
+  {
+    title: 'Easy to Use',
+    Svg: require('@site/static/img/moduops-pc.svg').default,
+    description: (
+      <>
+        Designed with modern user interface elements makes the software a joy to use.
+      </>
+    ),
+  },
+];
+
+function Feature({title, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--6')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
